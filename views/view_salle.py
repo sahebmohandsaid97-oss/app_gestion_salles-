43623576e96dbd13cd8ef3edefbae578a9c982f3
sahebmohandsaid97-oss <300150468@monboreal.ca>
2026-04-19 19:48:45 -0400

    Ajout frame Informations Salle

--- views/view_salle.py
+++ views/view_salle.py
@@ -12,4 +12,22 @@ class ViewSalle(ctk.CTk):
         self.geometry("600x400")
 
         self.service_salle = ServiceSalle()
+        self.frame_infos = ctk.CTkFrame(self)
+        self.frame_infos.pack(pady=10)
+
+        ctk.CTkLabel(self.frame_infos, text="Code").grid(row=0, column=0)
+        self.entry_code = ctk.CTkEntry(self.frame_infos)
+        self.entry_code.grid(row=0, column=1)
+
+        ctk.CTkLabel(self.frame_infos, text="Libelle").grid(row=1, column=0)
+        self.entry_libelle = ctk.CTkEntry(self.frame_infos)
+        self.entry_libelle.grid(row=1, column=1)
+
+        ctk.CTkLabel(self.frame_infos, text="Type").grid(row=2, column=0)
+        self.entry_type = ctk.CTkEntry(self.frame_infos)
+        self.entry_type.grid(row=2, column=1)
+
+        ctk.CTkLabel(self.frame_infos, text="Capacite").grid(row=3, column=0)
+        self.entry_capacite = ctk.CTkEntry(self.frame_infos)
+        self.entry_capacite.grid(row=3, column=1)
 
