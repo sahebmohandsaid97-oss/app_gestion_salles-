1aaf6ee6d4ab35de7729553084a24319c752a8ab
sahebmohandsaid97-oss <300150468@monboreal.ca>
2026-04-19 20:11:35 -0400

    Lancement interface graphique ViewSalle depuis main.py

--- main.py
+++ main.py
@@ -1,28 +1,4 @@
-from services.service_salle import ServiceSalle
-from models.salle import Salle
+from views.view_salle import ViewSalle
 
-
-service = ServiceSalle()
-
-
-print("Liste des salles :")
-salles = service.recuperer_salles()
-
-for salle in salles:
-    salle.afficher_infos()
-
-
-nouvelle_salle = Salle("B101", "Salle Test", "Classe", 40)
-
-succes, message = service.ajouter_salle(nouvelle_salle)
-print(message)
-
-
-nouvelle_salle.libelle = "Salle Test Modifiee"
-
-succes, message = service.modifier_salle(nouvelle_salle)
-print(message)
-
-
-succes, message = service.supprimer_salle("B101")
-print(message)
+app = ViewSalle()
+app.mainloop()
